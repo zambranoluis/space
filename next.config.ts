b46fd4b396ab3d28733bfbc6a709a6072ac6c283
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-12-04 20:34:50 -0400

    mnk

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['github.com'], // Agrega aquí los dominios permitidos
+  },
 };
 
 export default nextConfig;
