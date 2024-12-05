f73ca083112b3ae4f8360b0b4baab47821d84140
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-12-05 12:15:19 -0400

    mnk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,28 +31,28 @@ export default function Home() {
         
         <source src="https://github.com/BPM94/SCCTMD/raw/main/videoLogin.mp4" type="video/webm" />
       </video>
-      <div className="w-full h-full flex flex-col py-8 px-16 gap-4 h[500px] max-lg:bg-[white]/0 bg-[white] backdrop-blurmd z-[2] justify-center items-center rounded-md">
-        <div className=" flex justify-center items-center">
-          <Image className="w-[250px] drop-shadowanimate"
+      <div className=" lg:w-full lg:bg-white h-full flex flex-col py-8 px-16 gap4 max-lg:h-[500px] max-lg:bg[white]/100 bgwhite  z-[2] justify-center items-center rounded-md">
+        <div className=" flex justify-center items-center bg-white/100 px-2 rounded-lg bgred-200 w-full">
+          <Image className="w-[250px] drop-shadow[0_1.8px_1.8px_rgba(0,0,0,0.8)]  "
             width={786}
             height={318}
             src="https://github.com/BPM94/SCCTMD/raw/main/logoSpaceCreations.png"
             alt=""
           />
         </div>
+        {/* <div className="w-full  bggreen-300 flex justify-center items-center ">
+          <h1 className="text-4xl font-black text-white text-center drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]">Log In</h1>
+        </div> */}
         <div className="w-full  bgred-300 flex justify-center items-center ">
-          <form className=" flex flex-col p-2 gap-4 ">
-            <div>
-              <h1 className="text-4xl font-black text-black text-center">Log In</h1>
-            </div>
-            <div className="flex flex-col p-2 rounded-md gap-2 ">
+          <form className=" flex flex-col justify-center items-center p-2 gap-4 text-white lg:text-black w-full">
+            <div className="flex flex-col p-2 rounded-md gap-2 w-full max-w-[350px] ">
               <label
-                className="font-light text-black "
+                className="font-medium  "
                 htmlFor="email"
                 
               >Email:</label>
               <input
-                className="p-2 outline-none rounded-md "
+                className="p-2 outline-none w-full  text-black bg-white rounded-md border lg:border-black "
                 id="email"
                 type="text"
                 placeholder="Email"
@@ -60,14 +60,14 @@ export default function Home() {
                 
               />
             </div>
-            <div className="flex flex-col p-2 rounded-md gap-2 ">
+            <div className="flex flex-col p-2 rounded-md gap-2 w-full max-w-[350px]">
               <label
-                className="font-light text-black "
+                className="font-medium  "
                 htmlFor="password"
               >Password:</label>
               <div className="flex relative ">
                 <input 
-                  className="p-2 outline-none  rounded-md" 
+                  className="p-2 outline-none w-full  text-black bg-white  rounded-md border lg:border-black" 
                   id="password" 
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
