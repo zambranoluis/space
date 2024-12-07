b24aff9744148bff382071f430d90c703a3198d4
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-12-07 16:14:38 -0400

    mnk

--- src/app/(home)/home/page.jsx
+++ src/app/(home)/home/page.jsx
@@ -31,6 +31,27 @@ const pictures = [
   }
 ]
 
+const steps = [
+  {
+    id: 1,
+    image: "https://github.com/BPM94/SCCTMD/raw/main/step1.jpg",
+    title: "Step 1",
+    content: ["● Select your package", "● Talk to the experts about your budget", "● Take photos of your property", "● Tell us about your ideal space"]
+  },
+  {
+    id: 2,
+    image: "https://github.com/BPM94/SCCTMD/raw/main/step2.jpg",
+    title: "Step 2",
+    content: ["A landscape designer will work on your space to create a design according to your budget and style."]
+  },
+  {
+    id: 3,
+    image: "https://github.com/BPM94/SCCTMD/raw/main/step3.jpg",
+    title: "Step 3",
+    content: ["You will receive a file with images of your new space, 3d floor plan and the additional plans you have requested."]
+  }
+]
+
 
 function HomePage() {
 
@@ -72,15 +93,41 @@ function HomePage() {
 
 
       <section className="flex mt-[20px] flex-col ">
-        <div id="banner" className="w-full bg-[#848d5c] h-[200px] relative">
-          <p></p>
-          <p></p>
-          <div className="h-[100px] w-[100px] bg-[#848d5c] absolute bottom-[-50px] left-[10%] rotate-45"></div>
-        </div>
-        <div id="stepsContainer" className="w-full">
-          <div className=" h-[50dvh] bgblue-400 w-full" >
-
+        <div id="banner" className="w-full bg-[#848d5c] text-white  relative py-6 md:pl-16">
+          <div className="bgblue-200 max-w-[750px]  z-[100] flex flex-col px-6 gap-2">
+            <p className="text-5xl font-black ">
+              Bring your vision to life in three simple steps
+            </p>
+            <p className="text-lg">
+              Professional landscape design, always for a low flat fee
+            </p>
           </div>
+          <div className="h-[100px] w-[100px] bg-[#848d5c] z-[-1] absolute bottom-[-50px] left-[10%] rotate-45"></div>
+        </div>
+        <div id="stepsContainer" className="w-full  bgrose-400 p-2">
+          {
+            steps.map((step, index) => (
+              <div id={"step" + step.id} className={`${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} max-md:flex-col flex w-full bgblue-300 md:h-[50dvh] `}  key={index}>
+                <div id="stepTitle" className="flex max-md:h-[50dvh]  md:w-[50dvw] flex-col justify-center items-center">
+                  <div>
+                    <h1>{step.title}</h1>
+                  </div>
+                  <div>
+                  {
+                    step.content.map((content, index) => (
+                      <p key={index}>{content}</p> 
+                    ))
+                  }
+                  </div>
+                </div>
+                <div id="stepImage" className="flex max-md:h-[50dvh]  bg-yellow-300 md:w-[50dvw] justify-center items-center">
+                  <Image className="rounded-none w- h-[50dvh] md:w-[50dvw] " src={step.image} alt="" />
+                </div>
+              </div>
+              
+            ))
+
+          }
         </div>
       </section>
     </main>
