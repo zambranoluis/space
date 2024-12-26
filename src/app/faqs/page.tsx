e17f816f94b3c291467c43f775a4da031548a3db
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-12-26 15:10:38 -0400

    m

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,4 +1,13 @@
 'use client'
+import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalHeader,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
 
 import { useState } from "react";
 
@@ -11,84 +20,21 @@ import Footer from "@/components/Footer";
 
 import { SlArrowDown } from "react-icons/sl";
 
+import { MdEmail } from "react-icons/md";
+import { GrSkype } from "react-icons/gr";
+import { RiWhatsappFill } from "react-icons/ri";
 
 
-const faqs = [
-  {
-    id: 1,
-    question: "What is Space Creations?",
-    answer: " SpaceCreations is a studio specializing in landscape design for residential properties, transforming outdoor spaces into harmonious and functional environments. We have a team of expert designers who work closely with the client, understanding their needs and the context of the space to create innovative and personalized solutions. Our approach is to improve quality of life through well-designed landscapes that not only enhance the beauty of each residence but also optimize the relationship between the natural environment and architecture. At SpaceCreations, we are passionate about creating outdoor spaces that invite relaxation and enjoyment, while also adding aesthetic value and functionality to every home."
-  },
-  {
-    id: 2,
-    question: "What styles of landscape design do we offer?",
-    answer: "At SpaceCreations, we offer a wide variety of design styles to meet the preferences and needs of each client. Whether you're looking for a traditional and classic style, a modern and minimalist design, a zen and relaxing atmosphere, or the warm touch of a Mediterranean style, our team of experts is ready to create the perfect landscape that complements your residence. Every project is unique, and we ensure that the chosen style not only reflects your personality but also integrates harmoniously with the surroundings and architecture of your home."
-  },
-  {
-    id: 3,
-    question: "Can you create a custom design to suit my needs and budget?",
-    answer: "Yes, at SpaceCreations, we specialize in creating custom designs that fit both your needs and budget. We work closely with you to understand your expectations and provide solutions that maximize the potential of your space without compromising quality or your budget."
-  },
-  {
-    id: 4,
-    question: "Can I see examples of previous work or client references?",
-    answer: " Yes, you can visit our portfolio section, where you can see examples of previous work in the different design styles we have worked on. There, you will find a variety of projects that showcase the diversity of our approach and the quality of the designs we have created for our clients."
-  },
-  {
-    id: 5,
-    question: "Can I choose the plants and materials that will be used in the project?",
-    answer: " Yes, at SpaceCreations, we give you the opportunity to choose the plants and materials that will be used in your project. However, it is important that the plants you choose are suitable for the climate and conditions of the area where your residence is located. Our team will guide you to ensure that the options you select are the most appropriate to guarantee the success and durability of your landscape"
-  },
-  {
-    id: 6,
-    question: "Do you use native species or low - maintenance plants?",
-    answer: " Our main focus is on using native species, as they are better suited to the local climate, require less maintenance, and contribute to the sustainability of the ecosystem. However, we always ensure that we meet our clients' requirements and preferences, also integrating low-maintenance plants when necessary to achieve a functional and attractive design."
-  },
-  {
-    id: 7,
-    question: "How long will it take to complete the design?",
-    answer: " The delivery time for your design at SpaceCreations is usually around 2 weeks, although it may vary depending on the selected package and the complexity of the project. We work to ensure that we deliver a detailed, high-quality design within the agreed timeframe."
-  },
-  {
-    id: 8,
-    question: "What sets your company apart from others in the market?",
-    answer: "What sets SpaceCreations apart from other companies in the market is our personalized approach and attention to detail in every project. We specialize in creating landscapes that are not only aesthetically pleasing but also functional and sustainable. Our experience and passion for design, combined with the use of native species and high-quality materials, allow us to offer unique solutions tailored to the specific needs of each client. Additionally, we work closely with our clients throughout the entire process, ensuring that their vision and budget are respected."
-  },
-  {
-    id: 9,
-    question: "Is the staff working on the projects properly trained and experienced?",
-    answer: " Yes, all the staff working on our projects at SpaceCreations are properly trained and have extensive experience in landscape design. Our team consists of expert designers with deep knowledge in various areas, such as plant selection, space planning, and the use of sustainable materials. Additionally, we continuously work to stay updated with the latest trends and techniques in landscape design, ensuring quality and professionalism in every project."
-  },
-  {
-    id: 10,
-    question: "How is the initial consultation and project planning process carried out?",
-    answer: " The initial consultation and project planning process at SpaceCreations begins with a personalized meeting where we discuss your ideas, needs, and expectations. During this consultation, we make sure to understand your goals, style preferences, and any space constraints. We also conduct a detailed analysis of the environment and the climate of the area to ensure that the proposed solutions are suitable.  Based on this information, we create a preliminary plan and design proposal that aligns with your budget and vision. We then present the design to gather your feedback and make any necessary adjustments. Once approved, we move on to the execution and monitoring phase, ensuring that the project is carried out as agreed, with the quality and attention to detail that defines SpaceCreations."
-  },
-  {
-    id: 11,
-    question: "How do you handle communication during the project? Will I be regularly informed about the progress?",
-    answer: " Yes, we make sure to maintain constant and transparent communication throughout the entire project. We will communicate with you via Skype and email to keep you informed of any important details or updates. Additionally, you will receive notifications on our website, where you can track the progress of your design in real time. This way, you will always be informed and able to provide feedback or make adjustments as needed. "
-  },
-  {
-    id: 12,
-    question: "How do you handle revisions during the project?",
-    answer: " Each design package at SpaceCreations includes one round of revisions, where you can make adjustments and modifications to the design. If you want additional rounds of revisions, for an extra $100, you can request new modifications to ensure the final design meets all your expectations. We make sure to work flexibly so that you are completely satisfied with the result. "
-  },
-  {
-    id: 13,
-    question: "Do you have availability for short-term projects, or how far in advance are you booked?",
-    answer: "At SpaceCreations, we strive to be as flexible as possible with our timelines. While our schedule depends on the current workload, we generally have availability for short-term projects. We recommend getting in touch with us as soon as possible to coordinate the dates and ensure we can meet your deadlines without compromising the quality of the design. We would be happy to work with you and adapt to your needs."
-  },
-  {
-    id: 14,
-    question: "Are you willing to work with a tight budget and offer options to fit within it?",
-    answer: " Yes, we are willing to work with a tight budget and are committed to offering options that fit within it without sacrificing the quality of the design. Our team focuses on finding creative and efficient solutions that maximize the visual and functional impact of the space, always balancing your needs and budget. We ensure that each project remains accessible while still meeting the quality standards that define us. "
-  }
-]
+import { faqs } from "./faqs"
+
+import { FaqsContactUsEmail, FaqsContactUsSkype, FaqsContactUsWhatsapp } from "@/components/faqsContactUs";
+
 
 
 
 const CreateAccount = () => {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const toggleAnswer = (id: number) => {
     const container = document.getElementById(`answerContainer${id}`);
@@ -129,7 +75,7 @@ const CreateAccount = () => {
                 <div className="flex w-full bgrose-500 justify-between">
                   <h1 className="text-2xl md:text-3xl font-bold bgblue-800 py-4 px-4 border-l-8 border-[#6b776d] text-[#6b776d]">{faq.question}</h1>
                   <div className="flex bgblue-600 items-end">
-                    <SlArrowDown id={`arrow${faq.id}`} className="text-4xl font-light text-[#6b776d] cursor-pointer" onClick={() => {toggleAnswer(faq.id)}}/>
+                    <SlArrowDown id={`arrow${faq.id}`} className=" text-4xl font-light text-[#6b776d] cursor-pointer" onClick={() => {toggleAnswer(faq.id)}}/>
                   </div>
                 </div>
                 <div id={`answerContainer${faq.id}`} className="flex duration-1000 bgred-400">
@@ -138,6 +84,27 @@ const CreateAccount = () => {
               </div>
             ))
           }
+        </div>
+      </section>
+        
+      <section className="flex w-full justify-center items-center">
+        <div className="flex w-[60%] min-h-[300px]">
+          <div className="flex w-full max-w-[1000px]">
+            <Image className="w-full h-full rounded-r-none object-cover object-center" src="/mailFaq.jpg" alt="" />
+          </div>
+
+          <div className="bg-[#6b776d] rounded-r-xl text-white max-w-[400px]">
+            <div className="flex w-full p-6" >
+              <FaqsContactUsEmail />
+              {/* <FaqsContactUsSkype /> */}
+              {/* <FaqsContactUsWhatsapp /> */}
+            </div>
+            {/* <div className="flex w-full p-6" >
+              <FaqsContactUsEmail />
+              <FaqsContactUsSkype />
+              <FaqsContactUsWhatsapp />
+            </div> */}
+          </div>
         </div>
       </section>
 
---
+++ src/components/faqsContactUs.tsx
@@ -0,0 +1,172 @@
+'use client'
+import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalHeader,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
+
+import { useState } from "react";
+
+import { Image } from "@nextui-org/image";
+import Link from "next/link";
+import { MdEmail } from "react-icons/md";
+import { GrSkype } from "react-icons/gr";
+import { RiWhatsappFill } from "react-icons/ri";
+
+
+const FaqsContactUsEmail = () => {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <div>
+      <div className="flex flex-col">
+              <div className="flex">
+                <p className="">Let us know any particular questions you may have before or after you pay for any of our services and we will get back to you as soon as possible.</p>
+              </div>
+              <div className="flex w-full">
+                <button className="bgred-500 place-self-start"
+                onClick={onOpen}
+                >
+                  <MdEmail className="text-5xl" />
+                </button>
+                <Modal
+                
+                  size="xl"
+                  isOpen={isOpen}
+                  onOpenChange={onOpenChange}
+                >
+                  <ModalContent className=''>
+                    {(onClose) => (
+                      <div className="flex flex-col w-full p-10">
+                        <ModalHeader className="flex w-full bgred-400">
+                          <p className="text-sm font-medium text-left">
+                          Write any particular question you may have directly to us before or after you pay for any of our services and we will get back to you as soon as possible
+                          </p>
+                        </ModalHeader>
+                        <ModalBody>
+                          <form onSubmit={(e) => {e.preventDefault();}}>
+                            <div>
+                              <h2>Our Email: </h2>
+                              <p>space-creation@space-creation.net</p>
+                            </div>
+                            <div>
+                              <h2>Your Email: </h2>
+                              <input className="border-2 border-gray-300 rounded-md p-2" type="email" name="email" id="email" placeholder="Enter your email" required />
+                              
+                            </div>
+                            <div>
+                              <textarea className="w-full max-w-[500px] h-[250px] border-2 border-gray-300 rounded-md p-4" name="" id="" placeholder="Let us know your questions here..."></textarea>
+                            </div>
+                            <div>
+                              <button>
+                                send
+                              </button>
+                            </div>
+                          </form>
+                        </ModalBody>
+                        <ModalFooter>
+                        </ModalFooter>
+                      </div>
+                    )}
+                  </ModalContent>
+                </Modal>
+              </div>
+            </div>
+    </div>
+  );
+}
+
+const FaqsContactUsSkype = () => {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <div>
+
+            <div className="flex flex-col">
+              <div className="flex">
+                <p className="">Get in touch with us by Skype.</p>
+              </div>
+              <div className="flex w-full">
+                <button
+                className="bg-blue-500"
+                onClick={onOpen}
+                >
+                  <GrSkype className="text-5xl" />
+                </button>
+                <Modal
+                  size="xl"
+                  isOpen={isOpen}
+                  onOpenChange={onOpenChange}
+                >
+                  <ModalContent className=''>
+                    {(onClose) => (
+                      <div className="flex p-8">
+                        <ModalHeader>
+                          Write any particular question you may have directly to us before or after you pay for any of our services and we will get back to you as soon as possible
+                        </ModalHeader>
+                        <ModalBody>
+                        </ModalBody>
+                        <ModalFooter>
+                        </ModalFooter>
+                      </div>
+                    )}
+                  </ModalContent>
+                </Modal>
+              </div>
+            </div>
+
+          
+    </div>
+  );
+}
+
+const FaqsContactUsWhatsapp = () => {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <div className="flex">
+
+            <div className="flex flex-col">
+              <div className="flex">
+                <p className="">Get in touch with us by Whatsapp.</p>
+              </div>
+              <div className="flex w-full">
+                <button
+                className="bg-green-400"
+                onClick={onOpen}
+                >
+                  <RiWhatsappFill className="text-5xl" />
+                </button>
+                <Modal
+                  size="xl"
+                  isOpen={isOpen}
+                  onOpenChange={onOpenChange}
+                >
+                  <ModalContent className=''>
+                    {(onClose) => (
+                      <div className="flex p-8">
+                        <ModalHeader>
+                          Write any particular question you may have directly to us before or after you pay for any of our services and we will get back to you as soon as possible
+                        </ModalHeader>
+                        <ModalBody>
+                        </ModalBody>
+                        <ModalFooter>
+                        </ModalFooter>
+                      </div>
+                    )}
+                  </ModalContent>
+                </Modal>
+              </div>
+            </div>
+    </div>
+  );
+}
+
+export { FaqsContactUsEmail, FaqsContactUsSkype, FaqsContactUsWhatsapp };
