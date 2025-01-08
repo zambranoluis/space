b8c0fb6911a7421e512123f7e5c5e618aa513b4a
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2025-01-07 23:34:01 -0400

    m

--- src/app/(questionnaire)/questionnaire/page.tsx
+++ src/app/(questionnaire)/questionnaire/page.tsx
@@ -9,7 +9,9 @@ import { TiArrowSortedDown } from "react-icons/ti";
 import {Switch} from "@nextui-org/switch";
 
 
-
+import {
+  questionnaire
+} from "./questionnaire";
 
 
 
@@ -18,7 +20,7 @@ function ShoppingCart() {
   
 
   return (
-    <main className="w-full bgrose-400 flex flex-col gap-12">
+    <main className="w-full bgrose-400 flex flex-col gap-12 items-center">
       <section id="Landing" className="flex max-md:flex-col w-full md:h-[500px]">
         <div className="flex flex-col w-full md:w-[50%] bgred-200 p-2 justify-center gap-4 md:gap-16  py-12">
           <Link className="flex bggreen-300 p-2 min-lg:pl-12 justify-center items-center" href={"/shopping-cart"}>
@@ -36,53 +38,97 @@ function ShoppingCart() {
           </div>
         </div>
         <div className="flex w-full max-md:h-[500px]  md:w-[50%] bg-center bg-cover bg-no-repeat" style={{backgroundImage: "url('/questionnaire/questionnaireBGTop.webp')",}}>
-          {/* <Image className="rounded-none bg-center bg-cover bg-no-repeat" src="/questionnaire/questionnaireBGTop.webp"  alt=""/> */}
+          
         </div>
       </section>
-      <section id="generalQuestions" className="flex flex-col w-full bg-red-200 min-h-[500px]">
-        <div>
-          <div>
-            <div>
-              <h1>Tell us the area you want to design</h1>
-            </div>
+      <section id="generalQuestions" className="flex flex-col bgred-200 min-h-[500px] w-[90%] gap-12">
+        <div id="gq1" className="flex flex-col bgred-300 rounded-t-3xl border-2 border-[#858e5b]">
+          <div className="flex bg-[#858e5b] pt-4 pl-6 pb-2 text-xl font-black rounded-t-3xl">
+            <h1>{questionnaire["general"][0].title}</h1>
           </div>
-          <div>
-            <div>
-              <p>▪ Backyard</p>
-              <input type="checkbox" />
-            </div>
-            <div>
-              <p>▪ Frontyard</p>
-              <input type="checkbox" />
-            </div>
-            <div>
-              <p>▪ Both</p>
-              <input type="checkbox" />
-            </div>
-            <div>
-              <p>▪ Side Yard</p>
-              <input type="checkbox" />
-            </div>
-            
+          <div className="grid grid-cols-2 sm:grid-cols-4 py-12 gap-2 text-black">
+            {
+              questionnaire["general"][0].options.map((option, index) => (
+                <div className="flex bgblue-300 gap-2 w-full items-center justify-center " key={option.id}>
+                  <div className="w-[60%] bggreen-300 justify-end items-end flex">
+                    <p className="text-xs sm:text-base flex justify-center items-center gap-1"><span className="text-xl  text-[#68664d]">▪ </span>{option.name}</p>
+                  </div>
+                  <div className="w-[15%] bgyellow-200">
+                    <input className="text-[#ebebeb]" type="checkbox" />
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </div>
-        <div>
-          <div>
-            <div>
-              <h1>What style are you looking for your space?</h1>
+        <div id="gq2" className="flex flex-col bgred-300 rounded-t-3xl border-2 border-[#858e5b]">
+          <div className="flex bg-[#858e5b] pt-4 pl-6 pb-2 text-xl font-black rounded-t-3xl">
+            <h1>What style are you looking for your space?</h1>
+          </div>
+          <div className="w-full bgblue-300">
+            <div className="grid grid-cols-1  gap-2 py-6 min-[400px]:grid-cols-2 md:grid-cols-4 bggreen-300">
+              {
+                questionnaire["general"][1].options.map((option, index) => (
+                  <div className="bgred-300 flex flex-col justify-center items-center p-4" key={option.id}>
+                    <Image className="w-[90px] h-[90px] object-cover object-center rounded-full" src="/questionnaire/placeholder.webp" alt=""/>
+                    <div className="flex justify-center items-center gap-2 p-2">
+                      <p className="text-black flex justify-center items-center gap-1"><span className="text-xl text-[#68664d]">▪ </span>{option.name}</p>
+                      <input type="checkbox" />
+                    </div>
+                  </div>
+                ))
+              }
             </div>
-            <div>
-              <div>
-                <div>
-                  <Image src="" alt=""/>
-                </div>
+            <div id="gqOther" className="flex gap-2 w-full bgred-200 px-16 py-12 text-black">
+              <p className="text-[#68664d]">Other: </p>
+              <div className="border border-b-black border-b-2 w-full">
+                <input className="outline-none bg-white w-full text-black  pl-2" placeholder="" type="text" />
               </div>
-              <div className="flex gap-2">
-                <p>Other: </p>
-                <div className="border border-b-black">
-                  <input className="outline-none bg-white" placeholder="" type="text" />
-                </div>
-              </div>
+            </div>
+          </div>
+        </div>
+        <div id="gq3" className="flex flex-col bgred-300 rounded-t-3xl border-2 border-[#858e5b]">
+          <div className="flex bg-[#858e5b] pt-4 pl-6 pb-4 text-xl font-black rounded-t-3xl">
+            <h1>{questionnaire["general"][2].title}</h1>
+          </div>
+          <div className="flex relative h-[120px]">
+            <div className="flex justify-center items-center bgred-200 pl-2 min-[450px]:pl-8">
+              <button className="bg-[#ebebeb] rounded-tl-3xl rounded-br-3xl text-[#68664d] py-2 px-6">
+                Yes/No
+              </button>
+            </div>
+            <div className="bgred-300 h-full w-full absolute justify-end items-center flex pr-2 min-[450px]:pr-8 top-[-15px]">
+              <Image className="w-[120px] h-[120px] rounded-full" src={questionnaire["general"][2].img} alt="" />
+            </div>
+          </div>
+        </div>
+        <div id="gq4" className="flex flex-col bgred-300 rounded-t-3xl border-2 border-[#858e5b]">
+          <div className="flex bg-[#858e5b] pt-4 pl-6 pb-4 text-xl font-black rounded-t-3xl">
+            <h1>{questionnaire["general"][3].title}</h1>
+          </div>
+          <div className="flex relative h-[120px]">
+            <div className="flex justify-center items-center bgred-200 pl-2 min-[450px]:pl-8">
+              <button className="bg-[#ebebeb] rounded-tl-3xl rounded-br-3xl text-[#68664d] py-2 px-6">
+                Yes/No
+              </button>
+            </div>
+            <div className="bgred-300 h-full w-full absolute justify-end items-center flex pr-2 min-[450px]:pr-8 top-[-15px]">
+              <Image className="w-[120px] h-[120px] rounded-full" src={questionnaire["general"][3].img} alt="" />
+            </div>
+          </div>
+        </div>
+        <div id="gq5" className="flex flex-col bgred-300 rounded-t-3xl border-2 border-[#858e5b]">
+          <div className="flex bg-[#858e5b] pt-4 pl-6 pb-4 text-xl font-black rounded-t-3xl">
+            <h1>{questionnaire["general"][4].title}</h1>
+          </div>
+          <div className="flex relative h-[120px]">
+            <div className="flex justify-center items-center bgred-200 pl-2 min-[450px]:pl-8">
+              <button className="bg-[#ebebeb] rounded-tl-3xl rounded-br-3xl text-[#68664d] py-2 px-6">
+                Yes/No
+              </button>
+            </div>
+            <div className="bgred-300 h-full w-full absolute justify-end items-center flex pr-2 min-[450px]:pr-8 top-[-15px]">
+              <Image className="w-[120px] h-[120px] rounded-full" src={questionnaire["general"][4].img} alt="" />
             </div>
           </div>
         </div>
---
+++ src/app/(questionnaire)/questionnaire/questionnaire.ts
@@ -0,0 +1,97 @@
+const questionnaire = {
+  general: [
+    {
+      id: "qg1",
+      title: "Tell us the area you want to design",
+      options: [
+        {
+          id: "qg1o1",
+          name: "Backyard"
+        },
+        {
+          id: "qg1o2",
+          name: "Frontyard"
+        },
+        {
+          id: "qg1o3",
+          name: "Both"
+        },
+        {
+          id: "qg1o4",
+          name: "Side Yard"
+        }
+      ]
+    },
+    {
+      id: "qg2",
+      title: "What style are you looking for your space?",
+      options: [
+        {
+          id: "qg2o1",
+          name: "Backyard",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o2",
+          name: "Frontyard",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o3",
+          name: "Both",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o4",
+          name: "Side Yard",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o5",
+          name: "Rustic",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o6",
+          name: "Desert",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o7",
+          name: "Tropical",
+          img: "/questionnaire/placeholder.webp"
+        },
+        {
+          id: "qg2o8",
+          name: "Zen",
+          img: "/questionnaire/placeholder.webp"
+        }
+      ]
+    },
+    {
+      id: "qg3",
+      title: "Do you have kids & pets?",
+      img: "/questionnaire/placeholder.webp",
+    },
+    {
+      id: "qg4",
+      title: "Can toxic plants be used?",
+      img: "/questionnaire/placeholder.webp",
+    },
+    {
+      id: "qg5",
+      title: "Do you entertain a lot in your yard area?",
+      img: "/questionnaire/placeholder.webp",
+    }
+  ],
+  backyard: [
+
+  ],
+  frontyard: [
+
+  ]
+}
+
+export {
+  questionnaire
+}
