67bfba4256ba6f8acccec2b5271a14e85084c6c9
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2025-01-14 14:40:18 -0400

    m

--- src/components/NavbarClient.tsx
+++ src/components/NavbarClient.tsx
@@ -62,7 +62,7 @@ const Navbar: React.FC<NavbarProps> = ( { toggleAside } ) => {
       </nav>
 
 
-      <nav id="navbarLarge" className="max-[900px]:hidden flex top-0 bg-black/15 w-full z-[1500] h-[100px] bgred-300 justify-between items-center px-6  ">
+      <nav id="navbarLarge" className="max-[900px]:hidden flex top-0 bg-black/50 w-full z-[1500] h-[100px] bgred-300 justify-between items-center px-6  ">
         <div id="logoNavbar" className="flex bgred-200">
           <Image className="h-full w-full max-w-[250px] drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]"
             width={200}
@@ -72,28 +72,12 @@ const Navbar: React.FC<NavbarProps> = ( { toggleAside } ) => {
           />
         </div>
         <div id="titleNavbar" className="flex select-none bggreen-300">
-          <h1 className="text-[#69664c] text-2xl font-bold drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]">Projects</h1>
+          <h1 className="text-[#6b776d] text-2xl font-bold drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]">Projects</h1>
         </div>
         <div id="searchNavbar" className="flex gap-3 sm:gap-6 bgblue-300  max-w-[350px] lg:w-full">
           <input className="outline-none  pl-6 py-2 px-2 bg-[#6c6c6c] text-black rounded-2xl w-full" type="text" placeholder="Project search..." />
         </div>
-        <div id="notificationsNavbar" className="flex justify-center items-center gap-4  bgred-300">
-          <div className="bgred-300 cursor-pointer">
-            <Badge className="bg-[#fe2f2f] border-0 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]" content="" >
-              <div className="w-[30px] bgred-300 h-[30px] flex justify-center items-center" >
-                <FaCommentAlt className="text-xl text-[#6c6c6c] drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]" />
-              </div>
-            </Badge>
-          </div>
-          <div className="bgred-300 cursor-pointer">
-            <Badge className="bg-[#fe2f2f] border-0 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]" content="" >
-              <div className="w-[30px] bgred-300 h-[30px] flex justify-center items-center" >
-                <IoMdNotifications className="text-4xl text-[#6c6c6c] drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]" />
-              </div>
-            </Badge>
-          </div>
-        </div>
-        <div id="profileNavbar" className="flex justify-center items-center gap-2 text-[#8e7842] h-full bgred-300">
+        <div id="profileNavbar" className="flex justify-center items-center gap-2 text-[#6b776d] h-full bgred-300">
           <IoMdArrowDropdown className="text-2xl drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]" />
           <div className="flex flex-col justify-center items-center ">
             <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]">Seras Victoria</p>
