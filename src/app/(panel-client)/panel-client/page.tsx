6f942cc3c58f8d4bf4a0e76829141e38b5048ae9
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2025-01-16 23:41:38 -0400

    m

--- src/app/(panel-client)/panel-client/page.tsx
+++ src/app/(panel-client)/panel-client/page.tsx
@@ -65,7 +65,8 @@ function PanelClient() {
 
     try {
       const res = await axios.get<Customer>(
-        'https://715vq04v-4000.use2.devtunnels.ms:4000/space/customers/6789c1afce8c2f0ad7736d00'
+        'https://715vq04v-4000.use2.devtunnels.ms:4000/space/customers/6789c1afce8c2f0ad7736d00',
+        { withCredentials: true }
       );
       setCustomer(res.data); // Almacena los datos del cliente
       
