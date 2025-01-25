0eacecf36865dfd1c41b8781e80a3bff585e3d7c
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2025-01-25 14:53:51 -0400

    m

--- src/components/Purchases/Purchases.tsx
+++ src/components/Purchases/Purchases.tsx
@@ -3,6 +3,7 @@ import { apiService } from "@/services/apiService";
 import { GiCheckMark } from "react-icons/gi";
 import { PiTagSimpleFill } from "react-icons/pi";
 import { FaSearch } from "react-icons/fa";
+import { FaTags } from "react-icons/fa6";
 
 interface Area {
   nameArea: string;
@@ -30,6 +31,31 @@ interface Purchase {
   status: string;
 }
 
+
+
+const typePurchase = [
+  {
+    name: "All Purchases",
+    icon: <FaTags className="text-2xl"/>
+  },
+  {
+    name: "Pending",
+    icon: <img src={"/purchasePendingSpaceColorsGreen.png"} className="w-[25px] h-[25px]"/>,
+  },
+  {
+    name: "Development",
+    icon: <img src={"/purchaseDevelopmentColorsGreen.png"} className="w-[25px] h-[25px]"/>,
+  },
+  {
+    name: "Completed",
+    icon: <img src={"/purchaseCompletedColorsGreen.png"} className="w-[25px] h-[25px]"/>,
+  },
+  {
+    name: "Canceled",
+    icon: <img src={"/purchaseCanceledColorsGreen.png"} className="w-[25px] h-[25px]"/>,
+  },
+]
+
 export const Purchases: React.FC = () => {
   const [currentClients, setCurrentClients] = useState<string | null>("All Purchases");
   const [purchases, setPurchases] = useState<Purchase[]>([]);
@@ -40,7 +66,7 @@ export const Purchases: React.FC = () => {
       setIsLoadingPurchase(true);
       try {
         const response = await apiService.getPurchasesByCustomerId(
-          "675388347f312ad7cc0a2ba2",
+          "678b3cb754c8efd3f5677ee5",
         );
         if (response?.data) {
           console.log("Datos de las compras:", response.data);
@@ -73,15 +99,15 @@ export const Purchases: React.FC = () => {
   return (
     <div className='flex flex-col w-[90%] h-full place-self-center gap-2'>
       <div className='flex place-self-center border-[#6b776d] border-2 text-[#6b776d] rounded-md p2 w-[90%] max-w-[505px] h-[80px] overflow-x-scroll scrollbar-hide'>
-        {["All Purchases", "Pending", "Development", "Completed", "Canceled"].map(
-          (status, index) => (
+        {typePurchase.map(
+          (purchase, index) => (
             <div key={index} className='flex justify-center items-center p-1 w-[100px]'>
               <p
                 className={`w-full ${
-                  currentClients === status ? "border border-[#6b776d] rounded-md" : ""
+                  currentClients === purchase.name? "border border-[#6b776d] rounded-md" : ""
                 } text-xs flex flex-col text-center justify-center items-center p-2 cursor-pointer whitespace-nowrap`}
-                onClick={() => setCurrentClients(status)}>
-                {status}
+                onClick={() => setCurrentClients(purchase.name)}>
+                {purchase.icon}{purchase.name}
               </p>
             </div>
           ),
@@ -104,9 +130,9 @@ export const Purchases: React.FC = () => {
 
         <div className='grid overflow-y-scroll h-[90%] p-2 py-4 gap-6 w-full'>
           {purchases.map((purchase: Purchase, index: number) => (
-            <div key={index} className='flex flex-col p-2'>
-              <div
-                className='flex items-center gap-2 p-2 bg-[#6b776d] text-white justify-between border border-[#6b776d] rounded-t-md'
+            <div key={index} className='flex flex-col p-2 '>
+              <div id="purchaseTitle"
+                className='flex cursor-pointer items-center gap-2 p-2 bg-[#6b776d] text-white justify-between border border-[#6b776d] rounded-t-md'
                 onClick={() =>
                   document.getElementById(`purchase${index}`)?.classList.toggle("hidden")
                 }>
@@ -122,7 +148,7 @@ export const Purchases: React.FC = () => {
               <div
                 id={`purchase${index}`}
                 className='hidden flex flex-col bg-gray-100 text-black p-4 border border-[#6b776d]'>
-                <div className='flex flex-col'>
+                <div  className='flex flex-col '>
                   <h2 className='font-bold text-xl'>Package:</h2>
                   <p>
                     {purchase.product.name} {purchase.product.type}
@@ -141,19 +167,22 @@ export const Purchases: React.FC = () => {
                 </div>
                 <div className='flex flex-col'>
                   <h2 className='font-bold text-xl'>Extras:</h2>
-                  {purchase.extras.map((extra: Extra, i: number) => (
-                    <p key={i}>
-                      <GiCheckMark className='inline-block text-green-600' />{" "}
-                      {extra.extra.name}
-                    </p>
-                  ))}
+                  {
+                    purchase.extras.some((extra: Extra) => extra.isActive) ? (
+                      purchase.extras.map((extra: Extra, i: number) => (
+                        (extra.isActive) && <p key={i}><GiCheckMark className='inline-block text-green-600' />{" "}{extra.extra.name}</p>
+                      ))
+                    ) : (
+                      <p>No extra selected</p>
+                    )
+                  }
                 </div>
                 <div className='flex flex-col mt-4'>
                   <h2 className='font-bold text-xl'>Price: ${purchase.total}</h2>
-                  {purchase.status === "Pending" && (
+                  {purchase.status === "pending" && (
                     <button
                       onClick={() => handlePayment(purchase._id)}
-                      className='mt-2 px-3 py-2 bg-[#858e5b] text-white rounded-tl-3xl rounded-br-3xl'>
+                      className='mt-2 px-3 py-2 bg-[#858e5b] text-white place-self-start rounded-tl-3xl rounded-br-3xl'>
                       Process Payment
                     </button>
                   )}
